dadbc77cb109727cc37c913260da818ecc8da0ae
KouMoh <126945657+KouMoh@users.noreply.github.com>
2024-05-19 18:13:21 +0530

    Add files via upload

---
+++ select.sql
@@ -0,0 +1,14 @@
+SELECT* 
+FROM parks_and_recreation.employee_demographics;
+
+SELECT
+age, 
+age+10
+FROM 
+employee_demographics;
+
+SELECT DISTINCT
+gender,
+first_name
+FROM
+employee_demographics;
